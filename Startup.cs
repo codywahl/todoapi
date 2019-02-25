c94c023ec8ca2c3fc7922b18c2723949ec92ca98
Cody <codywahl@gmail.com>
2019-02-25 13:44:40 -0800

    added ignore

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 using TodoApi.Models;
 
 namespace TodoApi {
@@ -18,6 +19,11 @@ namespace TodoApi {
         public void ConfigureServices (IServiceCollection services) {
             services.AddDbContext<TodoContext> (opt => opt.UseInMemoryDatabase ("TodoList"));
             services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,8 +35,17 @@ namespace TodoApi {
                 app.UseHsts ();
             }
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger ();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI (c => {
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseDefaultFiles ();
+            app.UseStaticFiles ();
             app.UseHttpsRedirection ();
             app.UseMvc ();
         }
